f9766e417d8ed89dc296e3f7c31dd19e01d745d4
Hasharxd <MuhammadHashar72@gmail.com>
2025-06-17 12:54:43 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'ツ𝑯𝒂𝒔𝒉𝒂𝒓 Xd', // Your name
+  ownerNumber: '+92 370 1557437', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
